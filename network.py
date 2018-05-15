4741126f4590cb8248e93c87187056665629d92d
peternara <peternara@users.noreply.github.com>
2018-05-15 14:19:13 +0900

    Update network.py

--- network.py
+++ network.py
@@ -501,16 +501,18 @@ def main(argv=None):
                 saver.restore(sess, model_name)
 
                 print('continue trainging:')
-
+                
+                # FLAGS.restore_model_step = 0
                 for left_step_train in range(FLAGS.restore_model_step + 1, num_step_train + 1):
 
                     start_time = time.time()
 
                     # train
+                    # FLAGS.restore_model_epoch = 0 
                     print('epoch: ' + str(FLAGS.restore_model_epoch) + ', step: ' + str(left_step_train) + ', batch is ready.')
                     
                     # 2 triplet pair >  3x2 = 6 
-                    # 0:query, 1:positve, 2:negative1, 3:query, 4:positvie 5:negative2
+                    # 1 pair(0 :query, 1:positve, 2:negative1), 2 pair(3:query, 4:positvie 5:negative2)
                     # cf) 0==3, 1==4 image
                     x_batch = utils.get_one_batch(train_url_file,
                                                   FLAGS.batch_size, # 24
